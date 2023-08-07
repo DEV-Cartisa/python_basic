863c516f74344b39d0fe2813d61527e21d50cdd9
DEV-Cartisa <122188281+DEV-Cartisa@users.noreply.github.com>
2023-08-07 21:42:17 +0530

    Add files via upload

---
+++ madlib.py
@@ -0,0 +1,16 @@
+
+
+noun = input('Noun: ')
+plural_noun = input('Plural Noun: ')
+noun1 = input('Noun: ')
+verb1 = input('verb: ')
+
+madlib1 = f"Dont sit too close to the {nount}  -youll go blind."
+madlib2 = f"Well, if the other {plural_noun} jumped off the London {noun1}, would you do it, too?"
+madlib3 = f"PI neverr {verb1} when i was your age"
+
+
+
+print(madlib1)
+print(madlib2)
+print(madlib3)
