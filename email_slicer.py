d9a1255db841a445289e822edc0bb991b1a30f01
DEV-Cartisa <122188281+DEV-Cartisa@users.noreply.github.com>
2023-08-12 18:17:02 +0530

    Add files via upload

---
+++ email_slicer.py
@@ -0,0 +1,15 @@
+
+email = input("Enter your Email:")
+
+index = email.index('@')
+
+username = email[:index]
+domain = email[index:]
+
+print(f"Your username is {username} and domain is {domain}")
+
+
+
+
+
+
