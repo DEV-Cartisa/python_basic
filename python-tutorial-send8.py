416401181c3baaeaf9694b82e80feb9624cd2ce8
DEV Ed.006 <122188281+DEV-Cartisa@users.noreply.github.com>
2023-09-24 23:07:24 +0530

    Update python-tutorial-send8.py

--- python-tutorial-send8.py
+++ python-tutorial-send8.py
@@ -3,3 +3,18 @@
 
 
 
+game7 = [[0, 0, 0],
+         [0, 0, 0],
+         [0, 0, 0],]
+
+
+def game_board(player=0, row=0, column=0, just_display=False):
+    print('   0  1  2')
+    if not just_display: # if no input
+        game7[row][column] = player #?? didnt work without this [ belongs to player?]
+    for count, row in enumerate(game7):
+        print(count,row)
+
+
+game_board()
+game_board(player=1, row=2, column=1)
